2da34e9db0a227533b3ef43d97a779ddb86f7159
Kevin Van Cott <kevinvandy656@gmail.com>
2024-09-14 16:21:46 -0500

    add svelte project

---
+++ apps/5.1-sveltekit-ssr/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '../app.css';
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+				staleTime: 1000 * 10 // 10 seconds
+			}
+		}
+	});
+</script>
+
+<svelte:head>
+	<title>SvelteKit SSR (and Svelte Query)</title>
+	<meta
+		name="viewport"
+		content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+	/>
+	<link rel="shortcut icon" href="/favicon.svg" />
+</svelte:head>
+
+<QueryClientProvider client={queryClient}>
+	<slot></slot>
+</QueryClientProvider>

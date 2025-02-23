debe7036ab9b70b7645b2e6cca26bd16de9879d8
Kevin Van Cott <kevinvandy656@gmail.com>
2025-02-22 20:55:40 -0600

    use form submissions in astro ssr example

--- apps/3-ssr/3-4-astro-ssr/src/pages/api/add-comment.ts
+++ apps/3-ssr/3-4-astro-ssr/src/pages/api/add-comment.ts
@@ -5,7 +5,9 @@ export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData();
     console.log(formData);
     const body = formData.get("body");
-    const postId = new URL(request.url).searchParams.get("postId");
+    const url = new URL(request.url);
+    const postId = url.searchParams.get("postId");
+    const redirectHash = url.searchParams.get("redirectHash");
 
     if (!body || !postId) {
       return new Response("Missing required fields", { status: 400 });
@@ -28,10 +30,10 @@ export const POST: APIRoute = async ({ request }) => {
 
     if (!response.ok) throw new Error("Failed to create comment");
 
-    // Get the current origin from the request URL
-    const url = new URL(request.url);
-    const redirectUrl = new URL(`/posts/${postId}`, url.origin);
-
+    const redirectUrl = new URL(
+      `/posts/${postId}${redirectHash ? "#" + redirectHash : ""}`,
+      url.origin
+    );
     return Response.redirect(redirectUrl.toString(), 303);
   } catch (error) {
     console.error("Error creating comment:", error);
--- apps/3-ssr/3-4-astro-ssr/src/pages/api/delete-comment.ts
+++ apps/3-ssr/3-4-astro-ssr/src/pages/api/delete-comment.ts
@@ -5,6 +5,8 @@ export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData();
     const commentId = formData.get("commentId");
     const postId = formData.get("postId");
+    const url = new URL(request.url);
+    const redirectHash = url.searchParams.get("redirectHash");
 
     if (!commentId || !postId) {
       return new Response("Missing required fields", { status: 400 });
@@ -19,10 +21,10 @@ export const POST: APIRoute = async ({ request }) => {
 
     if (!response.ok) throw new Error("Failed to delete comment");
 
-    // Get the current origin from the request URL
-    const url = new URL(request.url);
-    const redirectUrl = new URL(`/posts/${postId}`, url.origin);
-
+    const redirectUrl = new URL(
+      `/posts/${postId}${redirectHash ? "#" + redirectHash : ""}`,
+      url.origin
+    );
     return Response.redirect(redirectUrl.toString(), 303);
   } catch (error) {
     console.error("Error deleting comment:", error);

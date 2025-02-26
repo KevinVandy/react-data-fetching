9246fc52e4397cd391813f141a446310f4f1c4f2
Kevin Van Cott <kevinvandy656@gmail.com>
2025-02-26 16:29:42 -0600

    comments

--- apps/2-ssg/2-2-astro-ssg/astro.config.mjs
+++ apps/2-ssg/2-2-astro-ssg/astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  adapter: vercel({
+    isr: {
+      expiration: 60 * 60 * 24, // rebuild every page every day
+    },
+  }),
 });
